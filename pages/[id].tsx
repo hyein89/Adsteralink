64b595456e1a6a85dbc8efaf793ee14288a9ad78
hyein89 <bihontai@gmail.com>
2025-10-26 04:25:38 +0700

    Update [id].tsx

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,3 +1,5 @@
+import fs from "fs";
+import path from "path";
 import { GetServerSideProps } from "next";
 
 type DataItem = {
@@ -9,8 +11,9 @@ type DataItem = {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { id } = context.params!;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL || "https://contoh.vercel.app"}/data.json`);
-  const data: DataItem[] = await res.json();
+  const filePath = path.join(process.cwd(), "public", "data.json");
+  const jsonData = fs.readFileSync(filePath, "utf-8");
+  const data: DataItem[] = JSON.parse(jsonData);
 
   const item = data.find((x) => x.id === id);
   if (!item) {
@@ -28,7 +31,7 @@ export default function RedirectPage({ item }: { item: DataItem }) {
         <meta name="description" content="" />
         <meta property="og:locale" content="en_GB" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content={`https://contoh.vercel.app/${item.id}`} />
+        <meta property="og:url" content={`https://adsteralink.vercel.app/${item.id}`} />
         <meta property="og:title" content={item.title} />
         <meta property="og:image" content={item.image} />
         <link
@@ -60,18 +63,18 @@ export default function RedirectPage({ item }: { item: DataItem }) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            setInterval(function(){
-              var num = document.getElementById("number").innerHTML;
-              if (num > 0) num--;
-              document.getElementById("number").innerHTML = num;
-              if (num == 0) {
-                var url1 = 'signingunwilling';
-                var url2 = '.com';
-                var url3 = '/haba8g98r5';
-                window.location.replace('https://' + url1 + url2 + url3 + "?key=${item.key}");
-              }
-            }, 1000);
-          `,
+              setInterval(function(){
+                var num = document.getElementById("number").innerHTML;
+                if (num > 0) num--;
+                document.getElementById("number").innerHTML = num;
+                if (num == 0) {
+                  var url1 = 'signingunwilling';
+                  var url2 = '.com';
+                  var url3 = '/haba8g98r5';
+                  window.location.replace('https://' + url1 + url2 + url3 + "?key=${item.key}");
+                }
+              }, 1000);
+            `,
           }}
         />
       </body>
