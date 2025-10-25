b5cee8919f8d5739ba774d0d4d40ec1291d3a8f2
hyein89 <bihontai@gmail.com>
2025-10-26 03:42:53 +0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        textAlign: "center",
+        padding: "50px",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1>Selamat Datang 👋</h1>
+      <p>Gunakan URL seperti <code>/abc123</code> untuk redirect otomatis.</p>
+      <p>Preview link juga akan muncul di Facebook dan WhatsApp.</p>
+    </main>
+  );
+}
